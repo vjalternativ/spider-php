194417b3f548e39995af16f13d5596eb779c5268
vijaykumar <vijaykumar@drishti-soft.com>
2019-11-19 23:44:39 +0530

    changes

--- include/utils.php
+++ include/utils.php
@@ -1,4 +1,31 @@
 <?php
+
+function saveMediaFile($field,$keyvalue) {
+    global $entity,$vjconfig;
+    $mediaKeyValue=array();
+    
+    if(!$keyvalue['isnew'] || !empty($keyvalue[$field['name']])) {
+        $mediaKeyValue = $entity->get("media_files",$keyvalue[$field['name']]);
+        if(isset($mediaKeyValue['file_path'])) {
+            
+            unlink($mediaKeyValue['file_path']);
+        }
+    }
+    $fileId = create_guid();
+    $dir = $vjconfig['basepath']."media_files/".date("Y").'/'.date("m").'/'.date("d").'/'.$_FILES[$field['name']]['type'];
+    if(!is_dir($dir)) {
+        mkdir($dir, 0755, true);
+    }
+    $target = $dir.'/'.$fileId;
+    $tmp = $_FILES[$field['name']]['tmp_name'];
+    move_uploaded_file($tmp, $target);
+    $mediaKeyValue['name'] =$_FILES[$field['name']]['name'];
+    $mediaKeyValue['file_path'] = $target;
+    $mediaKeyValue['file_type'] = $_FILES[$field['name']]['type'];
+    $mediaId  = $entity->save("media_files",$mediaKeyValue);
+    return $mediaId;
+}
+
 function redirect($module,$action=false,$params=array()) {
 	$string = "index.php?module=".$module;
 	if($action) {
