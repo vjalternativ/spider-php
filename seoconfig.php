a0c123553834f5250f5633c99eb4141e6a2605d6
vijaykumar <vijaykumar@drishti-soft.com>
2020-01-03 22:03:15 +0530

    changes

--- seoconfig.php
+++ seoconfig.php
@@ -1,62 +1,17 @@
 <?php
 
 global $seoConfig;
-$seoConfig['controlarea']['params']['module'] = "user";
-$seoConfig['controlarea']['params']['action'] = "login";
-$seoConfig['aboutus']['params']['entryPoint'] = "site";
-$seoConfig['aboutus']['params']['page'] = "aboutus";
-$seoConfig['aboutus']['child']['test']['params']['method'] = "test"; 
-$seoConfig['aboutus']['child']['sukul']['params']['method'] = "sukul";
+$seoConfig['example']['params']['module'] = "examplemodule";
+$seoConfig['example']['params']['action'] = "examplemoduleaction";
 
 
-$seoConfig['default']['params']['entryPoint'] = "site";
-$seoConfig['default']['params']['page'] = "page";
+$seoConfig['example2']['params']['entryPoint'] = "site";
+$seoConfig['example2']['params']['page'] = "example_page";
+
+$seoConfig['example2']['child']['example3']['params']['method'] = "examplemethod";
+$seoConfig['example2']['child']['example3']['params']['method'] = "examplemethod2";
 
 
-$seoConfig['search']['params']['entryPoint'] = "site";
-$seoConfig['search']['params']['page'] = "search";
-
-
-$seoConfig['checkout']['params']['entryPoint'] = "site";
-$seoConfig['checkout']['params']['page'] = "search";
-$seoConfig['checkout']['params']['method'] = "checkout";
-
-
-
-$seoConfig['bookingsummary']['params']['entryPoint'] = "site";
-$seoConfig['bookingsummary']['params']['page'] = "search";
-$seoConfig['bookingsummary']['params']['method'] = "bookingsummary";
-
-
-$seoConfig['category']['params']['entryPoint'] = "site";
-$seoConfig['category']['params']['page'] = "category";
-$seoConfig['category']['params']['method'] = "index";
-
-$seoConfig['testentrypoint']['params']['entryPoint'] = "test";
-
-$seoConfig['thankyou']['params']['entryPoint'] = "site";
-$seoConfig['thankyou']['params']['page'] = "search";
-$seoConfig['thankyou']['params']['method'] = "thankyou";
-
-
-
-$seoConfig['getSiteMap']['params']['entryPoint'] = "site";
-$seoConfig['getSiteMap']['params']['page'] = "page";
-$seoConfig['getSiteMap']['params']['method'] = "getSiteMap";
-
-
-$seoConfig['searchAirport']['params']['entryPoint'] = "site";
-$seoConfig['searchAirport']['params']['page'] = "home";
-$seoConfig['searchAirport']['params']['method'] = "searchAirport";
-
-
-$seoConfig['contactus']['params']['entryPoint'] = "site";
-$seoConfig['contactus']['params']['page'] = "contacts";
-
-
-$seoConfig['fbwebhook']['params']['page'] = "fbwebhook";
-$seoConfig['fbwebhook']['params']['entryPoint'] = "site";
-$seoConfig['fbwebhook']['child']['newLead']['params']['method'] = "newLead";
 
 
 
