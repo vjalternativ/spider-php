e19e87061ac95eb8f665406d1eb4259ee6ee17ab
vijaykumar <vijaykumar@drishti-soft.com>
2020-06-23 18:29:20 +0530

    changes

--- modules/media_files/MediaFilesService.php
+++ modules/media_files/MediaFilesService.php
@@ -9,27 +9,29 @@ trait MediaFilesService  {
     
     function saveMediaFileByFieldArray($field,$keyvalue) {
         global $entity,$vjconfig;
-        $mediaKeyValue=array();
-        
-        if(!$keyvalue['isnew'] || !empty($keyvalue[$field['name']])) {
-            $mediaKeyValue = $entity->get("media_files",$keyvalue[$field['name']]);
-            if(isset($mediaKeyValue['file_path'])) {
-                
-                unlink($mediaKeyValue['file_path']);
+        $mediaId = false;
+        if(isset($_FILES[$field['name']]) && $_FILES[$field['name']]['error']=='0') {
+            $mediaKeyValue=array();
+            if(!$keyvalue['isnew'] || !empty($keyvalue[$field['name']])) {
+                $mediaKeyValue = $entity->get("media_files",$keyvalue[$field['name']]);
+                if(isset($mediaKeyValue['file_path'])) {
+                    
+                    unlink($mediaKeyValue['file_path']);
+                }
             }
+            $fileId = create_guid();
+            $dir = $vjconfig['basepath']."media_files/".date("Y").'/'.date("m").'/'.date("d").'/'.$_FILES[$field['name']]['type'];
+            if(!is_dir($dir)) {
+                mkdir($dir, 0755, true);
+            }
+            $target = $dir.'/'.$fileId;
+            $tmp = $_FILES[$field['name']]['tmp_name'];
+            move_uploaded_file($tmp, $target);
+            $mediaKeyValue['name'] =$_FILES[$field['name']]['name'];
+            $mediaKeyValue['file_path'] = $target;
+            $mediaKeyValue['file_type'] = $_FILES[$field['name']]['type'];
+            $mediaId  = $entity->save("media_files",$mediaKeyValue);
         }
-        $fileId = create_guid();
-        $dir = $vjconfig['basepath']."media_files/".date("Y").'/'.date("m").'/'.date("d").'/'.$_FILES[$field['name']]['type'];
-        if(!is_dir($dir)) {
-            mkdir($dir, 0755, true);
-        }
-        $target = $dir.'/'.$fileId;
-        $tmp = $_FILES[$field['name']]['tmp_name'];
-        move_uploaded_file($tmp, $target);
-        $mediaKeyValue['name'] =$_FILES[$field['name']]['name'];
-        $mediaKeyValue['file_path'] = $target;
-        $mediaKeyValue['file_type'] = $_FILES[$field['name']]['type'];
-        $mediaId  = $entity->save("media_files",$mediaKeyValue);
         return $mediaId;
     }
     
