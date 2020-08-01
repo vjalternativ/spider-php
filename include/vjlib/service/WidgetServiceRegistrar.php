a62486342d823175a735ad3d00b34402557f1d4d
vijaykumar <vijaykumar@drishti-soft.com>
2020-08-01 12:31:03 +0530

    changes

--- include/vjlib/service/WidgetServiceRegistrar.php
+++ include/vjlib/service/WidgetServiceRegistrar.php
@@ -4,7 +4,7 @@ require_once $vjconfig['fwbasepath'].'include/vjlib/interface/IWidgetService.php
 require_once $vjconfig['fwbasepath'].'include/vjlib/libs/WidgetService.php';
 class WidgetServiceRegistrar {
     private static $instnace = null;
-    static function getInstance() {
+    static function getWidgetServiceInstance() {
         if(self::$instnace ==null) {
             self::$instnace = new WidgetService();
         }
