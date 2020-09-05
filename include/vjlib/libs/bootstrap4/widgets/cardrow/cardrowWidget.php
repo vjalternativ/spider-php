a2035ee949702082ee6a6a0c249a39f9c91155bc
vijaykumar <vijaykumar@drishti-soft.com>
2020-09-05 18:58:43 +0530

    refactor

--- include/vjlib/libs/bootstrap4/widgets/cardrow/cardrowWidget.php
+++ include/vjlib/libs/bootstrap4/widgets/cardrow/cardrowWidget.php
@@ -1,15 +1,14 @@
-<?php 
-
-require_once $vjconfig['fwbasepath'].'include/vjlib/abstract/AWidget.php';
+<?php
+$vjconfig = lib_config::getInstance()->getConfig();
 require_once $vjconfig['fwbasepath'].'include/vjlib/libs/bootstrap4/bootstrap4.php';
 require_once $vjconfig['fwbasepath'].'include/vjlib/libs/bootstrap4/IBootstrapWidgetConstant.php';
 
 class cardrowWidget extends AWidget {
-    
 
-    
-    
-    
+
+
+
+
     private $rows = 0;
     private $cards = 0;
     private $rowindex = 0;
@@ -20,7 +19,7 @@ class cardrowWidget extends AWidget {
     private $maxrows = 10;
     private $maxcards = 3;
     private $isSetSize = false;
-    
+
     public function processWidgetParams($params)
     {
         if(is_array($params)) {
@@ -29,37 +28,37 @@ class cardrowWidget extends AWidget {
                     $this->maxcards= $params['maxcards'];
                 }
                 if(isset($params['isSetSize'])) {
-                    $this->isSetSize = $params['isSetSize']; 
+                    $this->isSetSize = $params['isSetSize'];
                 }
-                
-                
+
+
                 foreach($params['items'] as $item) {
                     $this->addcard($item);
                 }
                 $params = array();
                 $params['title'] = $this->title;
                 $params['data'] = $this->data;
-                 
+
             }
        }
-       
+
         return $params;
     }
     function __construct($title="",$maxrow=10,$maxcards=3) {
-        $this->title=$title; 
+        $this->title=$title;
         $this->maxrows = $maxrow;
         $this->maxcards = $maxcards;
         $this->checkMaxRows = $maxrow;
     }
-    
-  
-    
-    
+
+
+
+
     function addcard($param=array("header"=>false,"body"=>true,"footer"=>true)) {
         $card = Bootstrap4::loadWidget(IBootstrapWidgetConstant::$CARD,$param);
-        
+
         if(!$this->checkMaxRows) {
-            $this->maxrows = $this->rowindex+1; 
+            $this->maxrows = $this->rowindex+1;
         }
         if($this->rowindex < $this->maxrows) {
             if($this->cardindex <  $this->maxcards) {
@@ -74,16 +73,16 @@ class cardrowWidget extends AWidget {
                 $this->cardindex  = 0;
             }
         }
-        
+
     }
-    
+
     function getWidget() {
         $param = array();
         $param['title'] = $this->title;
         $param['data'] = $this->data;
         return Bootstrap4::loadWidget(IBootstrapWidgetConstant::$CARDROW,$param);
     }
-    
-    
+
+
 }
 ?>
