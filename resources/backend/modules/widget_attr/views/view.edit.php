adbff63e317e90514ad77d39418f4cf4ed810466
vijaykumar <vijaykumar@drishti-soft.com>
2020-09-06 15:24:24 +0530

    reafactor

--- resources/backend/modules/widget_attr/views/view.edit.php
+++ resources/backend/modules/widget_attr/views/view.edit.php
@@ -1,35 +1,41 @@
 <?php
-require_once 'include/vjlib/abstract/AWidget.php';
 class widget_attrViewEdit extends ViewEdit {
-    
+
     function preDisplay() {
+
         parent::preDisplay();
-        
+
         $widgetId = false;
         if(isset($_REQUEST['parent_record'])) {
-            
+
             $widgetId = $_REQUEST['parent_record'];
         } else if($this->data && $this->data['widget_widget_attr_1_m']) {
             $widgetId = $this->data['widget_widget_attr_1_m'];
-            
+
             $json = json_decode($this->data['description'],1);
             foreach($json as $key=>$val) {
                 $this->data[$key] = $val;
             }
         }
         if($widgetId) {
-            
+
             $entity = lib_entity::getInstance();
-            
+
             $wdata = $entity->get("widget",$widgetId);
             if($wdata) {
-                
                 $wtype = $wdata['widget_type'];
-                
+
                 $vjconfig = lib_config::getInstance()->getConfig();
-                
-                $file = "include/vjlib/libs/bootstrap4/widgets/".$wtype."/".$wtype."Widget.php";
-                
+
+
+                require_once $vjconfig['fwbasepath']. 'resources/widget/WidgetServiceRegistrar.php';
+
+                $widgetService  = WidgetServiceRegistrar::getWidgetServiceInstance();
+
+                $widgetService->getWidget($widgetType);
+
+                $file = $vjconfig['fwbasepath']."include/vjlib/libs/bootstrap4/widgets/".$wtype."/".$wtype."Widget.php";
+
                 $isFound = false;
                 if(file_exists($file)) {
                     $isFound = true;
@@ -39,42 +45,42 @@ class widget_attrViewEdit extends ViewEdit {
                         $isFound = true;
                     }
                 }
-                
+
                 if($isFound) {
                     require_once $file;
                     $class = $wtype."Widget";
-                    
+
                     $this->def['fields']  =array();
-                    
+
                     $this->def['metadata']['editview']  =array();
-                    
+
                     $x =  new  $class;
                    // $x=new IWidget();
                     $fields = $x->getFields();
-                    
+
                     $nameField = array("name"=>"name","type"=>"varchar");
                     $this->def['fields']['name'] = $nameField;
                     $this->def['metadata']['editview']['name']['type'] = 'row';
                     $this->def['metadata']['editview']['name']['fields'][0]['gridsize'] = 6;
                     $this->def['metadata']['editview']['name']['fields'][0]['field'] = $nameField;
-                    
+
                     foreach($fields as  $field) {
                         $this->def['fields'][$field['name']]['name'] = $field['name'];
                         $this->def['fields'][$field['name']]['type'] = $field['type'];
                         $this->def['metadata']['editview'][$field['name']]['type'] = 'row';
                         $this->def['metadata']['editview'][$field['name']]['fields'][0]['gridsize'] = 6;
                         $this->def['metadata']['editview'][$field['name']]['fields'][0]['field'] = $field;
-                        
-                        
+
+
                     }
-                    
+
                 }
             }
         }
-        
-        
+
+
     }
-    
+
     function display() {
         parent::display();
     }
