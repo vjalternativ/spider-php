33ee7a494862ca93eb3585501b1ec5899306f847
vijaykumar <vijaykumar@drishti-soft.com>
2020-09-06 23:13:25 +0530

    reafactor

--- resources/backend/include/language/en_us.string.php
+++ resources/backend/include/language/en_us.string.php
@@ -6,5 +6,5 @@ $mod_string['LBL_NAME'] = 'Name';
 $mod_string['LBL_DESCRIPTION'] = 'Description';
 $mod_string['LBL_DATE_ENTERED'] = 'Created On';
 $mod_string['LBL_RELATED_RECORDS'] = 'Related Records';
-
+lib_datawrapper::getInstance()->set("app_list_strings_list",$mod_string);
 ?>
--- resources/backend/include/language/lang.php
+++ resources/backend/include/language/lang.php
@@ -1,63 +1,63 @@
 <?php $app_list_strings = array (
-  'relationship_type_list' => 
+  'relationship_type_list' =>
   array (
     '1_M' => 'One-To-Many',
     'M_1' => 'Many-To-One',
   ),
-  'tabletype_list' => 
+  'tabletype_list' =>
   array (
     'basic' => 'Basic',
     'relationship' => 'Relationship',
     'user' => 'User',
     'cstm' => 'Custom'
-      
+
   ),
-  'user_type_list' => 
+  'user_type_list' =>
   array (
     'superuser' => 'Super User',
     'admin' => 'Administrator',
     'guest' => 'Guest',
   ),
-  'role_access_list' => 
+  'role_access_list' =>
   array (
     'no' => 'no',
     'yes' => 'yes',
   ),
-  'test_list' => 
+  'test_list' =>
   array (
     'a' => 'A',
     'b' => 'B',
   ),
-  'layout_param_list' => 
+  'layout_param_list' =>
   array (
     'row' => 'Row',
     'hr' => 'HR',
   ),
-  'status_list' => 
+  'status_list' =>
   array (
     'Active' => 'Active',
     'Inactive' => 'Inactive',
     '' => '',
   ),
-  'page_list' => 
+  'page_list' =>
   array (
     'home' => 'home',
     'flight_page' => 'flight_page',
   ),
-  'position' => 
+  'position' =>
   array (
     'middle' => 'middle',
     'bottom' => 'bottom',
     'bottomline1' => 'bottomline1',
     'slider' => 'slider',
   ),
-  'pax_type' => 
+  'pax_type' =>
   array (
     'adult' => 'adult',
     'child' => 'child',
     'infant' => 'infant',
   ),
-  'title' => 
+  'title' =>
   array (
     'mr' => 'mr',
     'ms' => 'ms',
@@ -65,17 +65,19 @@
     'miss' => 'miss',
     'mstr' => 'mstr',
   ),
-  'trip_type_list' => 
+  'trip_type_list' =>
   array (
     1 => 'One Way',
     2 => 'Return Way',
     3 => 'Multistop',
   ),
    'runs_on_list' => array(
-       "all" => "all", 
+       "all" => "all",
        "new" => "new",
        "mod" => "modified"
-       
+
    )
-    
+
 );
+
+lib_datawrapper::getInstance()->set("app_list_strings_list",$app_list_strings);
