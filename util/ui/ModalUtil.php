6eafb21f1c2325589b1a1c41969eff9696906a51
vijaykumar <vijaykumar@drishti-soft.com>
2020-09-21 12:13:14 +0530

    refactor

--- util/ui/ModalUtil.php
+++ util/ui/ModalUtil.php
@@ -1,6 +1,6 @@
-<?php 
+<?php
 class ModalUtil {
-    static function getModal($id,$showheader,$title,$header,$bgclass,$body,$showfooter=false,$footercontent=false ,$display=false,$isFullScreen=false){
+    static function getModal($id,$class,$showheader,$title,$header,$bgclass,$body,$showfooter=false,$footercontent=false ,$display=false,$isFullScreen=false){
         $params = array();
         $params['id'] = $id;
         $params['showheader'] = $showheader;
@@ -10,7 +10,7 @@ class ModalUtil {
         $params['body'] = $body;
         $params['display'] = $display;
         $params['isfullscreen'] = $isFullScreen;
-        
+        $params['class'] = $class;
         if($showfooter) {
             $params['showfooter'] = $showfooter;
             $params['footerbutton'] = $footercontent;
@@ -18,14 +18,14 @@ class ModalUtil {
         $widgetService = WidgetServiceRegistrar::getWidgetServiceInstance();
         return $widgetService->getWidgetForParams("bs3modal", $params);
     }
-    
-    
+
+
     static function getBlockModalWithoutHeaderAndFooter($id,$body) {
        return self::getModal($id,false,false,false,false,$body,false,false ,true);
     }
-    
-    static function getBlockModalWithoutFooter($id,$title,$body,$isFullScreen=false) {
-        return self::getModal($id,true,$title,false,"primary",$body,false,false ,true,$isFullScreen);
+
+    static function getBlockModalWithoutFooter($id,$class,$title,$body,$isFullScreen=false) {
+        return self::getModal($id,$class,true,$title,false,"primary",$body,false,false ,true,$isFullScreen);
     }
 }
 ?>
