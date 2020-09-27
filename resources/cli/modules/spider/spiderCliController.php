06505a7a34432346bbf65f1d724c60b6234e000f
vijaykumar <vijaykumar@drishti-soft.com>
2020-09-27 16:34:21 +0530

    changes

--- resources/cli/modules/spider/spiderCliController.php
+++ resources/cli/modules/spider/spiderCliController.php
@@ -1,6 +1,8 @@
 <?php
 class spiderCliController extends CliResourceController {
     function action_install() {
+
+        require_once lib_config::getInstance()->get('fwbasepath').'resources/backend/modules/adminarea/SchemaDataPatcher.php';
         $datapatch = new SchemaDataPatcher();
         $datapatch->addRepairTable("user");
         $datapatch->repairFramework();
