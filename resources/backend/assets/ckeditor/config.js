916b5fd37981d4e7a9621d251f77bd5492da196d
vijaykumar <vijaykumar@drishti-soft.com>
2021-02-05 00:25:48 +0530

    changes

--- resources/backend/assets/ckeditor/config.js
+++ resources/backend/assets/ckeditor/config.js
@@ -9,4 +9,6 @@ CKEDITOR.editorConfig = function( config ) {
 	// config.uiColor = '#AADC6E';
 	config.extraPlugins = 'ckeditor_wiris';
 	
+	config.specialChars = config.specialChars.concat([  [ '\u20b9', 'Rupees' ]]);
+	
 };
