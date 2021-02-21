622be0e81a03c346af5854fa6419b000e87c7bdb
vijaykumar <vijaykumar@drishti-soft.com>
2021-02-21 12:39:15 +0530

    changes

--- libs/lib_smarty.php
+++ libs/lib_smarty.php
@@ -2,18 +2,12 @@
 $dir = __DIR__.'/';
 require_once $dir.'../thirdparty/server/Smarty/Smarty.class.php';
 class lib_smarty {
-
     private static $instance = null;
-
-
     static function getSmartyInstance() {
         if(self::$instance==null) {
             self::$instance = new Smarty();
         }
         return self::$instance;
     }
-
-
-
 }
 ?>
