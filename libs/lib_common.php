94c1d763a24224e8330ec8b82ffc57e7f09c8ad2
vijaykumar <vijaykumar@drishti-soft.com>
2021-03-05 17:04:09 +0530

    changes

--- libs/lib_common.php
+++ libs/lib_common.php
@@ -1,23 +1,28 @@
 <?php
 
-class lib_common {
+class lib_common
+{
 
-
-
-    function init($configPath) {
-        $dir = __DIR__.'/';
-        require_once $dir.'lib_logger.php';
-        require_once $dir.'lib_config.php';
-        require_once $dir.'lib_datawrapper.php';
-        require_once $dir.'lib_database.php';
-        require_once $dir.'lib_entity.php';
+    function init($configPath)
+    {
+        $dir = __DIR__ . '/';
+        require_once $dir . 'lib_logger.php';
+        require_once $dir . 'lib_config.php';
+        require_once $dir . 'lib_datawrapper.php';
+        require_once $dir . 'lib_database.php';
+        require_once $dir . 'lib_entity.php';
 
         $database = lib_database::getInstance();
-        $config = lib_config::getInstance($configPath)->get("database");
-        $database->connect($config['host'],$config['user'],$config['password'],$config['name']);
-
+        $config = lib_config::getInstance()->get("database");
+        $disableDb = lib_config::getInstance()->get("disabledb");
+        if (! $disableDb) {
+            try {
+                $database->connect($config['host'], $config['user'], $config['password'], $config['name']);
+            } catch (Exception $e) {
+                throw $e;
+            }
+        }
     }
-
 }
 
 ?>
