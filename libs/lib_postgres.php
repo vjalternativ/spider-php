b1d49a35707ff1b279fad29c8b9e878d38847a4c
vijaykumar <vijaykumar@drishti-soft.com>
2021-06-07 23:17:43 +0530

    changes

--- libs/lib_postgres.php
+++ libs/lib_postgres.php
@@ -1,34 +1,40 @@
 <?php
-class lib_postgres extends  lib_database {
+
+class lib_postgres extends lib_database
+{
+
     private $con;
-    private function throwConnectionError() {
+
+    private function throwConnectionError()
+    {
         throw new \Exception("postgres connection failed");
     }
+
     public function connect($host, $user, $pwd, $name)
     {
-        $string = "host= ".$host." ";
-        $string .= "user= ".$user." ";
-        if($pwd) {
-            $string .= "password= ".$user." ";
+        $string = "host= " . $host . " ";
+        $string .= "user= " . $user . " ";
+        if ($pwd) {
+            $string .= "password= " . $user . " ";
         }
-        $string .= "dbname= ".$name;
+        $string .= "dbname= " . $name;
         $this->con = pg_connect($string) or $this->throwConnectionError();
     }
 
-
     public function query($sql)
     {
-       return pg_query($this->con,$sql);
+        return pg_query($this->con, $sql);
     }
+
     public function fetch($qry)
     {
-            return pg_fetch_assoc($qry);
+        return pg_fetch_assoc($qry);
     }
 
     public function getrow($sql)
     {
         $qry = $this->query($sql);
-        if($qry) {
+        if ($qry) {
             return $this->fetch($qry);
         }
         return false;
@@ -38,16 +44,18 @@ class lib_postgres extends  lib_database {
     {
         pg_close($this->con);
     }
+
     public function getfields($table)
     {
-        $meta =   pg_meta_data($this->con,$table);
+        $meta = pg_meta_data($this->con, $table);
         $meta = array_keys($meta);
-        foreach($meta as  $key) {
+        foreach ($meta as $key) {
             $meta[$key] = $key;
         }
 
         return $meta;
     }
+
     public function createTable($table, $cols)
     {}
 
@@ -60,6 +68,7 @@ class lib_postgres extends  lib_database {
     public function tableExist($table)
     {}
 
-
+    public function getConnectionObject()
+    {}
 }
 ?>
