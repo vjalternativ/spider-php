cab24675e28cc4baba40376fe309c40b0f88e7b7
vijaykumar <vj.alternativ@gmail.com>
2021-09-05 22:42:13 +0530

    changes

--- libs/lib_common.php
+++ libs/lib_common.php
@@ -8,11 +8,13 @@ class lib_common
         $dir = __DIR__ . '/';
         require_once $dir . 'lib_logger.php';
         require_once $dir . 'lib_config.php';
+
+        $config = lib_config::getInstance($configPath)->get("database");
+
         require_once $dir . 'lib_datawrapper.php';
         require_once $dir . 'lib_database.php';
         require_once $dir . 'lib_entity.php';
 
-        $config = lib_config::getInstance($configPath)->get("database");
         $database = lib_database::getInstance();
         $disableDb = lib_config::getInstance()->get("disabledb");
         if (! $disableDb) {
