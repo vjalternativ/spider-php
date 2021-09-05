74f5c2d16c050c4e0eac6d72ce2cc0352f1a9c58
vijaykumar <vj.alternativ@gmail.com>
2021-09-05 22:06:29 +0530

    changes

--- libs/lib_util.php
+++ libs/lib_util.php
@@ -249,19 +249,18 @@ class lib_util
         return false;
     }
 
-    /*
-     * static function isset($array, ...$args)
-     * {
-     * $isValid = true;
-     * foreach ($args as $index) {
-     * if (! isset($array[$index])) {
-     * $isValid = false;
-     * break;
-     * }
-     * }
-     * return $isValid;
-     * }
-     */
+    static function isset($array, ...$args)
+    {
+        $isValid = true;
+        foreach ($args as $index) {
+            if (! isset($array[$index])) {
+                $isValid = false;
+                break;
+            }
+        }
+        return $isValid;
+    }
+
     static function slugify($text)
     {
         $text = str_replace("(", "", $text);
