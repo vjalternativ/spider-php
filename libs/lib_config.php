1aec4097f664467ca4e52d7001eb4c8f75d48215
vijaykumar <vj.alternativ@gmail.com>
2021-09-05 22:46:58 +0530

    changes

--- libs/lib_config.php
+++ libs/lib_config.php
@@ -9,7 +9,7 @@ class lib_config
 
     function __construct($configPath)
     {
-        $dir = $configPath ? $configPath : __DIR__;
+        $dir = __DIR__;
         $fwbasepath = str_replace("libs", "", $dir);
         if (isset($_SERVER['argv'])) {
             if (substr($_SERVER['SCRIPT_FILENAME'], 0, 1) == "/") {
