71216b473a1f51032a03d9236f2d3d3c2afdf71e
vjalternativ <vj.alternativ@gmail.com>
2021-11-21 22:53:13 +0530

    changes

--- libs/lib_curl.php
+++ libs/lib_curl.php
@@ -1,12 +1,12 @@
 <?php
 
-
 class lib_curl
 {
 
     private $responseCode;
 
     /**
+     *
      * @return mixed
      */
     public function getResponseCode()
@@ -15,6 +15,7 @@ class lib_curl
     }
 
     /**
+     *
      * @param mixed $responseCode
      */
     public function setResponseCode($responseCode)
@@ -22,68 +23,67 @@ class lib_curl
         $this->responseCode = $responseCode;
     }
 
-    function __construct() {
+    function __construct()
+    {}
 
-    }
+    /**
+     * This function will sync project module data with dcs with base 64 encode form
+     *
+     * @param String $method
+     * @param Array $parameters
+     * @param String $url
+     * @return mixed
+     */
+    function call($url, $method = "GET", $data = "", $headerList = array())
+    {
+        $curl = curl_init();
 
-	/**
-	 * This function will sync project module data with dcs with base 64 encode form
-	 * @param String $method
-	 * @param Array $parameters
-	 * @param String $url
-	 * @return mixed
-	 */
+        $options = array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 300,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => $method,
+            CURLOPT_HTTPHEADER => $headerList
+        );
 
-	function call($url,$method = "GET", $data= "",$headerList=array())
-	{
-	    $curl = curl_init();
+        if ($method == "POST") {
+            $options[CURLOPT_POSTFIELDS] = $data;
+        }
+        curl_setopt_array($curl, $options);
 
+        $response = curl_exec($curl);
+        $responseCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 
-	    $options  = array(
-	        CURLOPT_URL => $url,
-	        CURLOPT_RETURNTRANSFER => true,
-	        CURLOPT_ENCODING => "",
-	        CURLOPT_MAXREDIRS => 10,
-	        CURLOPT_TIMEOUT => 300,
-	        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-	        CURLOPT_CUSTOMREQUEST => $method,
-	        CURLOPT_HTTPHEADER =>$headerList
-	    );
-
-	    if($method=="POST") {
-	        $options[CURLOPT_POSTFIELDS] = $data;
-	    }
-	    curl_setopt_array($curl, $options);
-
-	    $response = curl_exec($curl);
-	    $responseCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-       
-	    $err = curl_error($curl);
-	    if($err) {
-	        print_r($err);die;
-	    }
-	    curl_close($curl);
+        $err = curl_error($curl);
+        if ($err) {
+            print_r($err);
+            die();
+        }
+        curl_close($curl);
 
         $this->setResponseCode($responseCode);
 
-	    return $response;
-	}
+        return $response;
+    }
 
-	function get($url,$header=array()) {
-	    $headerList = array();
-	    if($header) {
-	        foreach($header as $key=>$val) {
-	            $headerList[] = $key.':'.$val;
-	        }
-	    }
-	   return $this->call($url,"GET","",$headerList);
-	}
+    function get($url, $header = array())
+    {
+        $headerList = array();
+        if ($header) {
+            foreach ($header as $key => $val) {
+                $headerList[] = $key . ':' . $val;
+            }
+        }
+        return $this->call($url, "GET", "", $headerList);
+    }
 
-	function post($url,$payload) {
-        return $this->call($url,"POST",$payload);
-	}
-
+    function post($url, $payload, $headerList = array())
+    {
+        return $this->call($url, "POST", $payload, $headerList);
+    }
 }
-
 
 ?>
