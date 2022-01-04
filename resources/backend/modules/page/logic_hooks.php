28173a62dab877a390b21d1c5d97a8c74e4a9dce
vjalternativ <vj.alternativ@gmail.com>
2022-01-04 23:10:24 +0530

    changes

--- resources/backend/modules/page/logic_hooks.php
+++
@@ -1,8 +0,0 @@
-<?php
-$logicHook = lib_datawrapper::getInstance()->get("logichook_list");
-
-$logicHook['page']['before_save'] = array();
-$logicHook['page']['after_save'] = array();
-$logicHook['page']['before_save'][] = array("beforesave","include/hooks/aliashook.php","AliasLogicHook","beforeSave");
-lib_datawrapper::getInstance()->set("logichook_list",$logicHook);
-?>
