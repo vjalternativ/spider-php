5bff93f3fa18ba366e8f698ffcb70eb6460c42c2
vjalternativ <vj.alternativ@gmail.com>
2022-01-20 00:09:18 +0530

    changes

--- resources/backend/modules/media_files/MediaFilesServiceRegistrar.php
+++ resources/backend/modules/media_files/MediaFilesServiceRegistrar.php
@@ -1,23 +1,25 @@
 <?php
 $vjconfig = lib_config::getInstance()->getConfig();
-require_once $vjconfig['fwbasepath'].'resources/backend/modules/media_files/IMediaFilesService.php';
-require_once $vjconfig['fwbasepath'].'resources/backend/modules/media_files/MediaFilesService.php';
-class MediaFilesServiceRegistrar {
+require_once $vjconfig['fwbasepath'] . 'resources/backend/modules/media_files/IMediaFilesService.php';
+require_once $vjconfig['fwbasepath'] . 'resources/backend/modules/media_files/MediaFilesService.php';
+
+class MediaFilesServiceRegistrar
+{
 
     static $instance = null;
 
-    static function getInstance() {
-
-        if(self::$instance==null) {
-            $instance = new MediaFilesService();
+    static function getInstance()
+    {
+        if (self::$instance == null) {
+            $instance = MediaFilesService::getInstance();
         }
 
         return self::getIMediaFilesService($instance);
     }
 
-    private static function getIMediaFilesService(IMediaFilesService $ob) {
-        return  $ob;
+    private static function getIMediaFilesService(IMediaFilesService $ob)
+    {
+        return $ob;
     }
-
 }
 ?>
