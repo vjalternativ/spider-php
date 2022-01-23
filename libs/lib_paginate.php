ded6c6685668abfbe54def315c0eb45d50363dfb
vjalternativ <vj.alternativ@gmail.com>
2022-01-23 22:16:42 +0530

    changes

--- libs/lib_paginate.php
+++ libs/lib_paginate.php
@@ -1,179 +1,207 @@
 <?php
 
-class lib_paginate {
+class lib_paginate
+{
 
-	public $url='';
-	public $index=false;
-	public $noresult = 1;
-	public $endto = 1;
-	public $pagingnum=5;
-	public $sql=false;
-	public $array=true;
-	public $arrayindex=false;
-	public $db;
-	public $module;
-	public $action=false;
-	public $process = array();
-	public $processHook = array("instance"=>"","method"=>"","enumList"=>array());
-	public $extrafields = array();
-	private static $instance = null;
+    public $url = '';
 
-	function __construct() {
-	    $this->db = lib_database::getInstance();
-	}
+    public $index = false;
 
-	static function getInstance() {
-	    if(self::$instance==null) {
-	        self::$instance = new lib_paginate();
-	    }
-	    return self::$instance;
-	}
+    public $noresult = 1;
 
-	function setProcessHook($instance,$method) {
+    public $endto = 1;
 
+    public $pagingnum = 5;
 
+    public $sql = false;
 
-	    $this->processHook['instance'] = $instance;
-	    $this->processHook['method'] = $method;
+    public $array = true;
 
+    public $arrayindex = false;
 
-	    $this->db->setProcessHook($instance,$method);
-	}
+    public $db;
 
-	function process() {
+    public $module;
 
+    public $action = false;
 
-		$url=$this->url;
-		$index=$this->index;
-		$noresult = $this->noresult;
-		$endto = $this->endto;
-		$pagingnum=$this->pagingnum;
-		$sql=$this->sql;
+    public $process = array();
 
-		$array=$this->array;
-		$arrayindex=$this->arrayindex;
-		$db = $this->db;
+    public $processHook = array(
+        "instance" => "",
+        "method" => "",
+        "enumList" => array()
+    );
 
-		$counter  = 0;
-		$startfrom = 1;
-		$maxlimit = $noresult*$endto;
-		$page =  1;
-		$pageList = array();
-		$start = 0;
-		$next = 2;
-		$prev = 1;
-		$nexturl = $url.'2';
-		$prevurl = "#";
-		if($index) {
-			$page = $index;
-		}
-		if(isset($page) && $page>0) {
-			$start = ($page - 1)*$noresult;
-			$next = $page + 1;
-			$nexturl = $url.$next;
-		}
-		if(!$sql && (!$array || !is_array($array))) {
-			die('parameter 6 (sql) or parameter 7 $array is not set');
-		}
+    public $extrafields = array();
 
-		if($sql) {
-		$sql .= " LIMIT $start,$maxlimit";
+    private static $instance = null;
 
-		$qry = $db->query($sql);
-		$totalresult = $start+$qry->num_rows;
-		} else {
-			$totalresult = count($array);
-		}
-		$totalpages =  ceil($totalresult/$noresult);
+    function __construct()
+    {
+        $this->db = lib_database::getInstance();
+    }
 
-		if($page > 2) {
-			$startfrom = $page-2;
-			$endto = $page + 2;
-		}
+    static function getInstance()
+    {
+        if (self::$instance == null) {
+            self::$instance = new lib_paginate();
+        }
+        return self::$instance;
+    }
 
-		if($pagingnum) {
+    public function setArray($array)
+    {
+        $this->array = $array;
+    }
 
-		for($i=$startfrom;$i<=$totalpages;$i++) {
-			$counter++;
+    public function setNoresult($num)
+    {
+        $this->noresult = $num;
+    }
 
-			$pageList[] = $i;
+    public function setUrl($url)
+    {
+        $this->url = $url;
+    }
 
-			if($counter == $pagingnum || $counter==$endto) {
-				break;
-			}
+    public function setIndex($index)
+    {
+        $this->index = $index;
+    }
 
-		}
+    function setProcessHook($instance, $method)
+    {
+        $this->processHook['instance'] = $instance;
+        $this->processHook['method'] = $method;
 
-		}
-		if(isset($page) && $page>0) {
+        $this->db->setProcessHook($instance, $method);
+    }
 
+    function process()
+    {
+        $url = $this->url;
+        $index = $this->index;
+        $noresult = $this->noresult;
+        $endto = $this->endto;
+        $pagingnum = $this->pagingnum;
+        $sql = $this->sql;
 
-			if($page*$noresult >= $totalresult) {
-				$nexturl = "#";
-			}
+        $array = $this->array;
+        $arrayindex = $this->arrayindex;
+        $db = $this->db;
 
+        $counter = 0;
+        $startfrom = 1;
+        $maxlimit = $noresult * $endto;
+        $page = 1;
+        $pageList = array();
+        $start = 0;
+        $next = 2;
+        $prev = 1;
+        $nexturl = $url . '2';
+        $prevurl = "#";
+        if ($index) {
+            $page = $index;
+        }
+        if (isset($page) && $page > 0) {
+            $start = ($page - 1) * $noresult;
+            $next = $page + 1;
+            $nexturl = $url . $next;
+        }
+        if (! $sql && (! $array || ! is_array($array))) {
+            die('parameter 6 (sql) or parameter 7 $array is not set');
+        }
 
-			if($page>1) {
-				$prev = $page - 1;
-				$prevurl = $url.$prev;
+        if ($sql) {
+            $sql .= " LIMIT $start,$maxlimit";
 
-			}
+            $qry = $db->query($sql);
+            $totalresult = $start + $qry->num_rows;
+        } else {
+            $totalresult = count($array);
+        }
+        $totalpages = ceil($totalresult / $noresult);
 
-		}
+        if ($page > 2) {
+            $startfrom = $page - 2;
+            $endto = $page + 2;
+        }
 
+        if ($pagingnum) {
 
+            for ($i = $startfrom; $i <= $totalpages; $i ++) {
+                $counter ++;
 
-		$data = array();
-		$counter=0;
+                $pageList[] = $i;
 
-		if($sql) {
-		$sql = $this->sql." LIMIT $start,$maxlimit";
-		$processList = $this->process;
+                if ($counter == $pagingnum || $counter == $endto) {
+                    break;
+                }
+            }
+        }
+        if (isset($page) && $page > 0) {
 
+            if ($page * $noresult >= $totalresult) {
+                $nexturl = "#";
+            }
 
-		//$processList['name'] = array('a',"attr"=>array('href' => array('value'=>'index.php?module=')));
-		$data = $db->getrows($sql,'id',false,false,$processList);
+            if ($page > 1) {
+                $prev = $page - 1;
+                $prevurl = $url . $prev;
+            }
+        }
 
+        $data = array();
+        $counter = 0;
 
-		} else  {
-		    $rows = array_slice($array, $start,$maxlimit);
-			foreach($rows as $row) {
+        if ($sql) {
+            $sql = $this->sql . " LIMIT $start,$maxlimit";
+            $processList = $this->process;
 
+            // $processList['name'] = array('a',"attr"=>array('href' => array('value'=>'index.php?module=')));
+            $data = $db->getrows($sql, 'id', false, false, $processList);
+        } else {
+            $rows = array_slice($array, $start, $maxlimit);
+            foreach ($rows as $row) {
 
+                $counter ++;
+                if ($arrayindex && ! isset($row[$arrayindex])) {
+                    die("index not found in array");
+                } else if ($arrayindex && isset($row[$arrayindex])) {
+                    $data[$row[$arrayindex]] = $row;
+                } else {
+                    $data[] = $row;
+                }
+                if ($counter == $noresult) {
+                    break;
+                }
+            }
+        }
 
-				$counter++;
-				if($arrayindex && !isset($row[$arrayindex])) {
-					die("index not found in array");
-				}
+        $x = array(
+            "data" => $data,
+            "prevurl" => $prevurl,
+            "nexturl" => $nexturl,
+            "pagelist" => $pageList,
+            "activepage" => $this->index,
+            "url" => $this->url,
+            "next" => $next,
+            "prev" => $prev,
+            "resultperpage" => $noresult
+        );
+        return $x;
+    }
 
-				else if($arrayindex && isset($row[$arrayindex])) {
-					$data[$row[$arrayindex]] = $row;
-				}
-				else {
-					$data[] = $row;
-				}
-				if($counter==$noresult) {
-					break;
-				}
-			}
+    function getPagingHtml($pageinfo, $ajaxMode = false)
+    {
+        $smarty = lib_smarty::getSmartyInstance();
+        $vjconfig = lib_config::getInstance()->getConfig();
 
+        $smarty->assign('pageinfo', $pageinfo);
+        $smarty->assign('ajaxMode', $ajaxMode);
 
-		}
-
-		$x =  array("data"=>$data,"prevurl"=>$prevurl,"nexturl"=>$nexturl,"pagelist"=>$pageList,"activepage"=>$this->index,"url"=>$this->url,"next" => $next,"prev"=>$prev,"resultperpage"=>$noresult);
-		return $x;
-	}
-
-	function getPagingHtml($pageinfo,$ajaxMode=false) {
-
-	       $smarty= lib_smarty::getSmartyInstance();
-           $vjconfig= lib_config::getInstance()->getConfig();
-
-	       $smarty->assign('pageinfo',$pageinfo);
-	       $smarty->assign('ajaxMode',$ajaxMode);
-
-
-	       return $smarty->fetch($vjconfig['fwbasepath']."resources/backend/include/tpls/paginghtml.tpl");
-	}
+        return $smarty->fetch($vjconfig['fwbasepath'] . "resources/backend/include/tpls/paginghtml.tpl");
+    }
 }
 ?>
